1d545c9def568ccbb149f6a5e5f770f3f71f0005
J1NY0UNG <jinyoung3635@gmail.com>
2018-09-03 23:51:47 +0900

    js 연동 및 타이머 제작

--- server/static/index.js
+++ server/static/index.js
@@ -1,1 +1,22 @@
-alert('hello world');
+var SetTime = 900;		
+
+function msg_time() {	
+	
+	m = "00:"+ Math.floor(SetTime / 60) + ":" + (SetTime % 60);	
+	
+	var msg = m ;
+	
+	document.all.ViewTimer.innerHTML = msg;		
+			
+	SetTime--;					
+	
+	if (SetTime < 0) {			
+		
+		clearInterval(tid);		
+		alert("종료");
+	}
+	
+}
+
+window.onload = function TimerStart(){ tid=setInterval('msg_time()',1000) };
+
