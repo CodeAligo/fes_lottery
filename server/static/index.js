2f33162bb7f9127bd56e7c60fdadc87014f09692
looma1303 <kmj13033@gmail.com>
2018-09-04 16:37:43 +0900

    js,css 타이머 수정

--- server/static/index.js
+++ server/static/index.js
@@ -1,22 +1,32 @@
-var SetTime = prompt("원하는 시간을 적어주세요(분 씩으로)","600");
+var SetTime = prompt("원하는 시간을 적어주세요(분 씩으로)","10");
+SetTime = SetTime * 60
 
-function msg_time() {	
-	
-	m = "00:"+ Math.floor(SetTime / 60) + ":" + (SetTime % 60);	
-	
-	var msg = m ;
-	
-	document.all.ViewTimer.innerHTML = msg;		
-			
-	SetTime--;					
-	
-	if (SetTime < 0) {			
-		
-		clearInterval(tid);		
-		alert("종료");
+function msg_time() {
+	n = Math.floor(SetTime/60)
+	if (n < 10) {
+		n = "0" + String(n)
 	}
-	
+
+	y = SetTime % 60
+	if (y < 10) {
+		y = "0"+String(y)
+	}
+	m = "00:"+ n + ":" + y;
+
+	var msg = m ;
+
+
+	document.all.ViewTimer.innerHTML = msg;
+
+	SetTime--;
+
+	if (SetTime < 0) {
+
+		clearInterval(tid);
+		alert("종료");
+
+	}
+
 }
 
 window.onload = function TimerStart(){ tid=setInterval('msg_time()',1000) };
-
