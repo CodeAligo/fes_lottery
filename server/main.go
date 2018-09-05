9eade61db4884d2e375e546865f7429d3eecbed4
Junhwan Beom <solkblte@icloud.com>
2018-09-05 23:44:42 +0900

    Go, CSS, JS, HTML

--- server/main.go
+++ server/main.go
@@ -1,46 +1,69 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 type Result struct {
-	Number  []int
-	Winners int
+	Numbers    []string
+	NumWinners int
+}
+
+type Timer struct {
+	NextTime int
+	Msg      string
 }
 
 var (
 	Port   string
 	result Result
+	timer  Timer
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	result.Number = []int{}
-	result.Winners = 0
+func logger(r *http.Request) {
+	log.Println("Request:", r.URL.Path, "| From", r.RemoteAddr, "\n NextTime =", timer.NextTime, "NumWinners = ", result.NumWinners, "\n")
+}
 
-	if r.URL.Path[1:] == "a" {
-		result.Number = []int{24, 9, 8, 7}
-		result.Winners = 25
-	}
+func resultHandler(w http.ResponseWriter, r *http.Request) {
+	result.Numbers = []string{"24", "09", "08", "07"}
+	result.NumWinners = 25
 
-	log.Println("Request:", r.URL.Path, "| From", r.RemoteAddr)
-	t := template.Must(template.ParseFiles("template/main.html"))
+	logger(r)
+	t := template.Must(template.ParseFiles("template/result.html"))
 	t.Execute(w, result)
 }
 
+func timerHandler(w http.ResponseWriter, r *http.Request) {
+	logger(r)
+	t := template.Must(template.ParseFiles("template/timer.html"))
+	t.Execute(w, timer)
+}
+
 func main() {
-	if len(os.Args) == 1 {
-		panic("No port")
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: ./server port nexttime")
+		fmt.Println("Eg.    ./server 8080 10")
+		return
 	}
 	Port = ":" + os.Args[1]
-	log.Println("Staring Server... Port", Port)
+
+	var err error
+	timer.NextTime, err = strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("nexttime is not int")
+		return
+	}
+	log.Println("Staring Server... Port", Port, "\n")
 
 	static := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", static))
-	http.HandleFunc("/", index)
+	http.HandleFunc("/result", resultHandler)
+	http.HandleFunc("/timer", timerHandler)
 	http.ListenAndServe(Port, nil)
 
 }
--- server/static/index.js
+++ server/static/index.js
@@ -1,10 +1,3 @@
-var nextTime = prompt("목표 시간은?","10");
-target = new Date()
-target.setHours(nextTime, 0, 0, 0)
-now = new Date()
-SetTime = target - now
-SetTime = Math.floor(SetTime / 1000)
-
 function msg_time() {
     hour = 0
 	min = Math.floor(SetTime/60)
@@ -14,31 +7,42 @@ function msg_time() {
     }
 
     if (hour < 10) {
-        hour = "0" + String(hour)
+        hour = "0" + hour
     }
         
 	if (min < 10) {
-		min = "0" + String(min)
+		min = "0" + min
 	}
 
 	sec = SetTime % 60
 	if (sec < 10) {
-		sec = "0"+String(sec)
+		sec = "0" + sec
 	}
-	remainedTime = hour + ":"+ min + ":" + sec;
+	remainedTime = hour + ":" + min + ":" + sec;
 
 
 	document.all.ViewTimer.innerHTML = remainedTime;
 
 	SetTime--;
 
-	if (SetTime < 0) {
 
+	if (SetTime <= 0) {
 		clearInterval(tid);
-		alert("종료");
-
+        window.location.href = "result"
+        return
 	}
+
+    if (SetTime == 300) {
+        document.all.Msg.innerHTML = "⚠️ 이번 회차 구매가 마감되었습니다."
+    }
 
 }
 
-window.onload = function TimerStart(){ tid=setInterval('msg_time()',1000) };
+function start_timer() {
+    target = new Date()
+    target.setHours(nextTime, 0, 0, 0)
+    now = new Date()
+    SetTime = target - now
+    SetTime = Math.floor(SetTime / 1000)
+    window.onload = function TimerStart(){ tid=setInterval('msg_time()',1000) };
+}
