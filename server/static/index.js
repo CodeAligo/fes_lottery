c5d1a52d88a157729f1fe94a5b09c940bb4d1bca
J1N0UNG <jinyoung3635@gmail.com>
2018-09-11 22:51:18 +0900

    js 파일 주석 ^^ 할게 없다

--- server/static/index.js
+++ server/static/index.js
@@ -6,24 +6,24 @@ function msg_time() {
         hour++
     }
 
-    if (hour < 10) {
+    if (hour < 10) {                // 타이머 시간(hour)
         hour = "0" + hour
     }
         
-	if (min < 10) {
+	if (min < 10) {                 // 타이머 분(min)
 		min = "0" + min
 	}
 
-	sec = SetTime % 60
+	sec = SetTime % 60              // 타이머 초(se)
 	if (sec < 10) {
 		sec = "0" + sec
 	}
-	remainedTime = hour + ":" + min + ":" + sec;
+	remainedTime = hour + ":" + min + ":" + sec;    // 시간 : 분 : 초
 
 
-	document.all.ViewTimer.innerHTML = remainedTime;
+	document.all.ViewTimer.innerHTML = remainedTime;   // id = ViewTimer로 HTML 텍스트에 적용함
 
-	SetTime--;
+	SetTime--;   // 시간 감속
 
 
 	if (SetTime <= 0) {
@@ -32,14 +32,14 @@ function msg_time() {
         return
 	}
 
-    if (SetTime == 300) {
-        document.all.Msg.innerHTML = "⚠️ 이번 회차 구매가 마감되었습니다."
+    if (SetTime == 300) {                        
+        document.all.Msg.innerHTML = "⚠️ 이번 회차 구매가 마감되었습니다."   // SetTime = 300 ⚠️ '이번 회차 구매가 마감되었습니다.' 출력
     }
 
 }
 
 function start_timer() {
-    target = new Date()
+    target = new Date()                                 //서버 GO 에서 입력 받은 시간(hour) 값의 남은 시간 출력
     target.setHours(nextTime, 0, 0, 0)
     now = new Date()
     SetTime = target - now
@@ -48,8 +48,8 @@ function start_timer() {
 }
 
 
-function load_NumWinners() {
-	$.ajax({
+function load_NumWinners() {       
+	$.ajax({                       // 남은 수령자의 값을 서버 GO에 정보 요청
         type:"GET",
         url:"/ajax",
         dataType:"text",
