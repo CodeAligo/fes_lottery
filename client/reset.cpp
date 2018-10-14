4c17e8707119c9362d081f76296f2bd0bfb3d85e
KooYs09 <andys2004@naver.com>
2018-10-14 21:41:57 +0900

    입력 방식 변경

--- client/reset.cpp
+++ client/reset.cpp
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
-using namespace std;
-
 int main()
 {
 	freopen("student.txt", "w", stdout);
-	int i;
-	for(i=1; i<=31000; i++) printf("0 0\n");
-	
-	return 0;
+	for(int i=1; i<=4000; i++) printf("0 0\n");
 }
