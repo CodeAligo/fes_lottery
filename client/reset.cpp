a65adc9d75cd89609e2a8490dd95283d99e692de
KooYs09 <andys2004@naver.com>
2018-11-21 13:19:41 +0900

    파일 초기화 방식 변경

--- client/reset.cpp
+++ client/reset.cpp
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <conio.h>
+#include <windows.h>
 
 int main()
 {
-	freopen("student.txt", "w", stdout);
-	for(int i=1; i<=4000; i++) printf("0 0\n");
+	FILE *fp = fopen("student.txt", "w");
+	for(int i=1; i<=4000; i++) fprintf(fp, "0 0\n");
+	fclose(fp);
+	
+	printf("¿Ï·á");
+	Sleep(500);
+	printf(".");
+	Sleep(500);
+	printf(".");
+	Sleep(500);
+	printf(".");
+	Sleep(1000);
+	printf("!");
+	Sleep(500);
+	getch();
+	return 0;
 }
