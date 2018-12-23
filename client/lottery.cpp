775ce9ba93841e9925b850c4fecb8384c6881ce0
KooYs09 <andys2004@naver.com>
2018-12-24 02:39:38 +0900

    졸려

--- client/lottery.cpp
+++ client/lottery.cpp
@@ -1,6 +1,3 @@
-/*
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
@@ -35,6 +32,7 @@ int main()
 	file_input();
 	
 	int mode, i, log_con;
+	char a;
 	
 	while(1)
 	{
@@ -45,8 +43,9 @@ int main()
 			if(stu[stu_num].pw[0] == 0) add_pw();
 			else break;
 		}
+		file_output();
 		
-		for(i=1; i<=3; i++)
+		for(i=1; i<=3; i++)		// 비밀번호 체크 
 		{
 			if(check_pw()) {
 				log_con = 1;
@@ -54,24 +53,27 @@ int main()
 				break;
 			}
 			else print(9);
-			Sleep(1500);
+			Sleep(1000);
 		}
-		
 		if(log_con == 0) {
 			print(11);
 			continue; 
-		}
+		}	
 		
-		printf("\n");
+/*		printf("\n");
 		for(i=1; i<=stu[stu_num].pw[0]; i++) printf("%c", stu[stu_num].pw[i]);
 		printf("\n");
 		for(i=1; i<=checkpw_s; i++) printf("%c", checkpw[i]);
-		getch();
+		getch();*/
+		
+		print(13);
+		Sleep(10000);
 		
 		file_output();
 		
 		print(1);
 		mode = input_one();
+		
 		if(mode == 65 || mode == 97) {
 			print(10);
 			printf("Lottery");
@@ -120,7 +122,7 @@ int file_output()
 
 int print(int type)
 {
-	int i;
+	int i, j;
 	
 	system("cls");
 	printf("[ staff : %s ]\n\n", staff);
@@ -143,7 +145,7 @@ int print(int type)
 			printf("<로그인>\n\n    학년 : %d\n      반 : %d\n    번호 : %d\n\n확인(\'Y\'es/\'N\'o).. ", stu_num/1000, (stu_num%1000)/100, stu_num%100);
 			break;
 		case 6:		// 신규, 패스워드 설정 
-			printf("<패스워드>\n\n패스워드 설정\n4자리 이상의 숫자 또는 영문자로 구성된 패스워드 입력\n>> ");
+			printf("<패스워드>\n\n패스워드 설정\n4자리 이상20자리 이하의 숫자 또는 영문자로 구성된 패스워드 입력\n>> ");
 			for(i=1; i<=stu[stu_num].pw[0]; i++) printf("* ");		
 			break;
 		case 7:		// 로그인,  패스워드 입력 
@@ -158,8 +160,7 @@ int print(int type)
 		case 9:
 			print(7);
 			printf("\n\n<!!> 틀렸습니다.\a\n");
-			Sleep(2000);
-			print(10);
+			Sleep(1000);
 			break;
 		case 10:	// 조건 성립 확인용 
 			printf("    ");
@@ -175,7 +176,8 @@ int print(int type)
 		case 11:
 			print(7);
 			printf("\n\n<!!> 3회 틀렸습니다. 잠시 후 로그인 화면으로 돌아갑니다.\a\n");
-			Sleep(2500);
+			Sleep(1500);
+			print(10);
 			break;
 		case 12:
 			print(7);
@@ -183,6 +185,24 @@ int print(int type)
 			Sleep(1000);
 			print(10);
 			break;
+		case 13:	// 로또 확인 
+			printf("<로또>\n\n");
+			for(i=1; i<=stu[stu_num].lot[0][0]; i++)
+			{
+				printf("  [ %d ]\n", i);
+				printf("  =========================\n");
+				printf("  ");
+				for(j=1; j<=4; j++) printf("|  %d  ", j);
+				printf("|\n");
+				printf("  -------------------------\n");
+				printf("  ");
+				for(j=1; j<=4; j++) stu[stu_num].lot[i][j]<10?printf("| %2d  ", stu[stu_num].lot[i][j]):printf("| %3d ", stu[stu_num].lot[i][j]);
+				printf("|\n");
+				printf("  =========================\n\n\n");
+			}
+			break;
+		case 14: 	// 로또 입력 
+			
 		default:
 			break;
 	}
@@ -191,11 +211,12 @@ int print(int type)
 
 int input_one()
 {
-	int a;
-	a = getch();
-	if(a == 8) return 0;
-	printf("%c", a);
-	return a;
+	int a, b;
+	while(1)
+	{
+		a = getch();
+		if((65 <= a && a <= 90) || (97 <= a && a <= 122)) return a;
+	}
 }
 
 int input_num(int type)
@@ -246,6 +267,7 @@ int check_student()
 
 int add_num()
 {
+	
 	file_output();
 }
 
@@ -279,7 +301,7 @@ int add_pw()
 
 int check_pw()
 {
-	int a, i;
+	int a, i, f=0;
 	checkpw_s = 0;
 	print(7);
 	while(1)
@@ -295,10 +317,14 @@ int check_pw()
 		print(7);
 	}
 	
-	for(i=1; i<=checkpw_s; i++)
+	for(i=1; i<=stu[stu_num].pw[0]; i++)
 	{
-		if(checkpw[i] != stu[stu_num].pw[i]) return 0;	// 패스워드 불일치 
+		if(checkpw[i] == stu[stu_num].pw[i]) f=1;
+		else {
+			f=0;
+			break;
+		}
 	}
 	
-	return 1;			// 패스워드 일치 
+	return f;			// f=1 : 패스워드 일치, f=0 : 패스워드 불일치  
 }
