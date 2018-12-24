379e2c3811f0eeb5a53adfc30c32db9fe1c99c2a
KooYs09 <andys2004@naver.com>
2018-12-25 01:14:54 +0900

    입력 완성

--- client/lottery.cpp
+++ client/lottery.cpp
@@ -6,10 +6,10 @@
 using namespace std;
 
 struct data{
-	int check, pw[25], lot[6][5];	//	pw[0] : password length
+	int pw[25], lot[6][5];	//	pw[0] : password length
 };
 
-int stu_num, checkpw[25]={0, }, checkpw_s=0;
+int stu_num, checkpw[25]={0, }, checkpw_s=0, add_n=0;
 char staff[30];
 data stu[4001];
 
@@ -66,9 +66,6 @@ int main()
 		for(i=1; i<=checkpw_s; i++) printf("%c", checkpw[i]);
 		getch();*/
 		
-		print(13);
-		Sleep(10000);
-		
 		file_output();
 		
 		print(1);
@@ -76,14 +73,15 @@ int main()
 		
 		if(mode == 65 || mode == 97) {
 			print(10);
-			printf("Lottery");
-			//add_num();
-			break;
+			print(13);
+			printf("\n	다음으로 넘어가려면 아무 키나 누르세요.");
+			getch();
+			add_n=0;
+			add_num();
 		}
 		else if(mode == 66 || mode == 98) {
 			print(10);
 			printf("PW");
-			break;
 		}
 		else print(1);
 		
@@ -100,9 +98,9 @@ int file_input()
 	int i, j;
 	for(i=1; i<=4000; i++)
 	{
-		fscanf(fi, "%d %d", &stu[i].check, &stu[i].pw[0]);
+		fscanf(fi, "%d %d", &stu[i].lot[0][0], &stu[i].pw[0]);
 		for(j=1; j<=stu[i].pw[0]; j++) fscanf(fi, "%d", &stu[i].pw[j]);
-		for(j=1; j<=stu[i].check; j++) fscanf(fi, "%d %d %d %d", &stu[i].lot[1], &stu[i].lot[2], &stu[i].lot[3], &stu[i].lot[4]);
+		for(j=1; j<=stu[i].lot[0][0]; j++) fscanf(fi, "%d %d %d %d", &stu[i].lot[j][1], &stu[i].lot[j][2], &stu[i].lot[j][3], &stu[i].lot[j][4]);
 	}
 	fclose(fi);
 }
@@ -113,9 +111,10 @@ int file_output()
 	int i, j;
 	for(i=1; i<=4000; i++)
 	{
-		fprintf(fo, "%d ", stu[i].check);
+		fprintf(fo, "%d ", stu[i].lot[0][0]);
 		for(j=0; j<=stu[i].pw[0]; j++) fprintf(fo, "%d ", stu[i].pw[j]);
-		for(j=1; j<=stu[i].check; j++) fprintf(fo, "%d %d %d %d ", stu[i].lot[1], stu[i].lot[2], stu[i].lot[3], stu[i].lot[4]);
+		for(j=1; j<=stu[i].lot[0][0]; j++) fprintf(fo, "%d %d %d %d ", stu[i].lot[j][1], stu[i].lot[j][2], stu[i].lot[j][3], stu[i].lot[j][4]);
+		fprintf(fo, "\n");
 	}
 	fclose(fo);
 }
@@ -123,6 +122,12 @@ int file_output()
 int print(int type)
 {
 	int i, j;
+	
+	if(type == 15) {
+		printf("\n");
+		for(i=add_n+2; i<=4; i++) printf("  (%d) : \n", i);
+		return 0;
+	}
 	
 	system("cls");
 	printf("[ staff : %s ]\n\n", staff);
@@ -145,7 +150,7 @@ int print(int type)
 			printf("<로그인>\n\n    학년 : %d\n      반 : %d\n    번호 : %d\n\n확인(\'Y\'es/\'N\'o).. ", stu_num/1000, (stu_num%1000)/100, stu_num%100);
 			break;
 		case 6:		// 신규, 패스워드 설정 
-			printf("<패스워드>\n\n패스워드 설정\n4자리 이상20자리 이하의 숫자 또는 영문자로 구성된 패스워드 입력\n>> ");
+			printf("<패스워드>\n\n패스워드 설정\n4자리 이상  20자리 이하의 숫자 또는 영문자로 구성된 패스워드 입력\n>> ");
 			for(i=1; i<=stu[stu_num].pw[0]; i++) printf("* ");		
 			break;
 		case 7:		// 로그인,  패스워드 입력 
@@ -187,6 +192,8 @@ int print(int type)
 			break;
 		case 13:	// 로또 확인 
 			printf("<로또>\n\n");
+			if(stu[stu_num].lot[0][0] == 0) printf("\n	등록된 로또가 없습니다.\n\n\n");
+			
 			for(i=1; i<=stu[stu_num].lot[0][0]; i++)
 			{
 				printf("  [ %d ]\n", i);
@@ -202,7 +209,19 @@ int print(int type)
 			}
 			break;
 		case 14: 	// 로또 입력 
-			
+			printf("<로또>\n\n");
+			for(i=1; i<=add_n; i++) printf("  (%d) : %d\n", i, stu[stu_num].lot[stu[stu_num].lot[0][0]][i]);
+			printf("  (%d) : ", add_n+1);
+			break;
+		case 16:	// 로또 에러 
+			printf("<로또>\n\n");
+			for(i=1; i<=add_n; i++) printf("  (%d) : %d\n", i, stu[stu_num].lot[stu[stu_num].lot[0][0]][i]);
+			for(i=add_n+1; i<=4; i++) printf("  (%d) : \n", i);
+			printf("\n\n	<!!> 번호 형식이 잘못 되었습니다.\a\n");
+			break;
+		case 17:	// 로또 최대 
+			printf("<로또>\n\n\n	<!!> 로또를 5개 구매하셨습니다.\n	최대 개수만큼 구매하여 더 이상 구매 하실 수 없습니다.\n\n");
+			break;
 		default:
 			break;
 	}
@@ -211,7 +230,7 @@ int print(int type)
 
 int input_one()
 {
-	int a, b;
+	int a;
 	while(1)
 	{
 		a = getch();
@@ -223,6 +242,7 @@ int input_num(int type)
 {
 	int a, k=0;
 	print(type);
+	if(type == 14) print(15);
 	while(1)
 	{
 		a = getch();
@@ -231,6 +251,7 @@ int input_num(int type)
 		else if(48 <= a && a <= 57) k = k*10 + (a-48);
 		print(type);
 		k == 0 ? printf(" ") : printf("%d", k);
+		if(type == 14) print(15);
 	}
 }
 
@@ -267,8 +288,40 @@ int check_student()
 
 int add_num()
 {
+	int a, i, j, f=1;
+	if(stu[stu_num].lot[0][0] == 5) {
+		for(i=3; i>=1; i--)
+		{
+			print(17);
+			printf("%d초 후 로그인 창으로 넘어갑니다.", i);
+			Sleep(1000);
+		}
+	}
+	stu[stu_num].lot[0][0]++;
+	print(14);
+	print(15);
+	while(1)
+	{
+		a = input_num(14);
+		if(a == 0) {
+			f=0;
+			break;
+		}
+		else if(0 > a || a > 30) print(16);
+		else stu[stu_num].lot[stu[stu_num].lot[0][0]][++add_n] = a;
+		Sleep(500);
+		if(add_n == 4) break;
+	}
 	
+	if(1-f) stu[stu_num].lot[0][0]--;
 	file_output();
+	
+	for(i=5; i>=1; i--)
+	{
+		print(13);
+		printf("%d초 후 로그인 창으로 넘어갑니다.", i);
+		Sleep(1000);
+	}
 }
 
 int add_pw()
